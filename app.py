eb1ac6d6b00735519509925b14b286c5eaaacb98
Lokesh <dandamudilokesh1234@gmail.com>
2023-01-29 01:54:33 +0530

    first

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template,redirect,flash
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+## Load the model
+model = pickle.load(open('regmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+
+def predict_api():
